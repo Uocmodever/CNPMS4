865177be0483666f59b6cd8d4fdc183500d99dfe
Anh Cuong <anhcuong@example.com>
2023-12-13 09:48:43 +0700

    Add property email into file user.cs

--- models/user.cs
+++ models/user.cs
@@ -3,10 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 namespace git1.models
 {
     internal class user
     {
+        public int Email { get; set; }
     }
 }
